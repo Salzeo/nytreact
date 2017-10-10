e1a8b37ffd339cc0132c72a865550722be41893f
Sally Bradfield <salzeo@gmail.com>
2017-10-09 18:09:39 -0600

    more updates

--- nyt/src/App.js
+++ nyt/src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 class App extends Component {
   render() {
     return (
--- nyt/src/index.js
+++ nyt/src/index.js
@@ -6,3 +6,14 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+ // res.render('index', { title: 'Express'}); this is the old line
+  res.render('index.html');
+});
+
+module.exports = router;
